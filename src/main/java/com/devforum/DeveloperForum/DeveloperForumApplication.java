123b62e1c2fbddd979c90b829f190a39de276dba
Mustafa <mustafa841034@gmail.com>
2024-07-04 18:03:46 +0300

    initial commit

---
+++ src/main/java/com/devforum/DeveloperForum/DeveloperForumApplication.java
@@ -0,0 +1,13 @@
+package com.devforum.DeveloperForum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloperForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devforum/DeveloperForum/DeveloperForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devforum.DeveloperForum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloperForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
